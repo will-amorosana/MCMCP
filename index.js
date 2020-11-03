2ae058ef74ee618894cc03c3423a08c187c0e619
will-amorosana <will.amorosana@gmail.com>
2020-11-03 14:06:26 +0000

    Objects!
    Moved primary functionality to Chain class, and implemented cleaner testing functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+import Chain from "Chain.js";
+
+var chain_a = new Chain(100, 20);
+var chain_b = new Chain(100, 20);
+var chain_c = new Chain(100, 20);
+
+while(true){
+    //TODO: Make it iterate.
+}
