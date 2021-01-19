08c346694ba5a1b29472d61a1e21f208506c4308
will-amorosana <will.amorosana@gmail.com>
2021-01-19 14:01:35 +0000

    Added auto-reject and arbitrary number of chains

--- src/index.ts
+++ src/index.ts
@@ -6,38 +6,27 @@
 //This is how coordinates are passed between methods. It's basically just a tuple that can check if it's beyond bounds [0,350].
 // const ParamTypes = Object.freeze({ USER_DEFINED: 1, EMPTY: 2, AUTO_REJECT: 3 });
 
-enum ParamTypes {
-    UserDefined,
-    Empty,
-    AutoReject,
-}
+
 
 class Params {
     x: number;
     y: number;
-    paramType = ParamTypes.UserDefined;
 
-    constructor(x: number, y: number, type: ParamTypes) {
-        this.paramType = type;
-        if (this.paramType == ParamTypes.UserDefined) {
+    constructor(x: number, y: number) {
+
             this.x = x;
             this.y = y;
-        } else {
-            this.x = -1;
-            this.y = -1;
-        }
     }
 
     auto_copy() {
         let copy = JSON.parse(JSON.stringify(this));
-        copy.param_type = ParamTypes.AutoReject;
         return copy;
     }
 
     prop(variance: number) {
         const x = this.x + this.box_mueller(variance);
         const y = this.y + this.box_mueller(variance);
-        return new Params(x, y, ParamTypes.UserDefined);
+        return new Params(x, y);
     }
 
     box_mueller(variance: number) {
@@ -59,42 +48,56 @@ class Params {
     }
 }
 
-//Chains hold their own proposal variance and an array of Params as results.
+//A new result is added for each choice the user makes, and for each automatic rejection.
+class Result {
+    chosen: Params;
+    rejected: Params;
+    auto: boolean;
+
+    constructor(yes: Params, no: Params, auto_rejected: boolean) {
+        this.chosen = yes;
+        this.rejected = no;
+        this.auto = auto_rejected;
+    }
+
+    value(){
+        return this.chosen;3
+    }
+}
+
 //Chains hold their own proposal variance, their previous results, and their current run.
 class Chain {
     prop_var: number;
-    current_run: Params[];
-    results: Params[];
+    current_run: Result[];
+    results: Result[];
     old_head: Params;
     constructor(prop_var: number = 10) {
         this.prop_var = prop_var;
-        this.old_head = new Params(-1, -1, ParamTypes.Empty);
+        this.old_head = null;
         this.current_run = [];
         this.results = [];
     }
 
-    addPoint(x: Params) {
+    addPoint(x: Result) {
         //Adds a new result to the end of the array
         this.current_run.push(x);
     }
 
-    state() {
+    state() { //TODO: draw from DB
         //Returns the most recent point in the current run. If it's an empty array, returns null (behavior handled below)
         if (this.current_run.length == 0) {
             //If this is the first iteration of the current run, check for previous runs
-            if (this.old_head.paramType != ParamTypes.Empty) {
-                return this.old_head; //If there is one, forward its last point.
-            } else return null; //Otherwise tell it to scratch it.
+            return this.old_head; //May return NULL if this is there haven't been any former accepted runs
         } else {
             //If it's not the first step in the run, return the head of the current run
-            return this.current_run[this.current_run.length - 1];
+            return this.current_run[this.current_run.length - 1].value();
         }
     }
 
     update(ok: boolean) {
         if (ok) {
             //If it's all good, append the results and change the old_head.
-            this.old_head = this.current_run[this.current_run.length - 1];
+            this.old_head = this.current_run[this.current_run.length - 1].value();
             this.results.push(...this.current_run);
             this.current_run = [];
         } else {
@@ -106,9 +109,8 @@ class Chain {
 
 
 class Main {
-    chain_a: Chain;
-    chain_b: Chain;
-    chain_c: Chain;
+
+    chains: Chain[];
     c1: HTMLCanvasElement;
     c2: HTMLCanvasElement;
     panel1: CanvasRenderingContext2D;
@@ -118,14 +120,20 @@ class Main {
     current_chain: Chain;
     inputs: boolean[];
     iters: number;
+    //Checkout prevention hyperparameters
+    k: number;
+    t: number;
 
-    constructor() {
+    constructor(num_chains, iters, k, t, prop_var) {
         //Initialize Chains
-        this.iters = 100;
-        this.chain_a = new Chain(20);
-        this.chain_b = new Chain(10);
-        this.chain_c = new Chain(5);
-        this.current_chain = this.chain_a;
+        this.iters = iters;
+        this.k = k;
+        this.t = t;
+        this.chains = [];
+        for(let i = 0; i < num_chains; i++){
+            this.chains.push(new Chain(prop_var));
+        }
+        this.current_chain = this.chains[0];
 
         //Get references for HTML elements
         this.c1 = document.getElementById("canvas_1") as HTMLCanvasElement;
@@ -133,8 +141,8 @@ class Main {
         this.panel1 = this.c1.getContext("2d");
         this.panel2 = this.c2.getContext("2d");
 
-        this.side1 = new Params(-1, -1, ParamTypes.Empty);
-        this.side2 = new Params(-1, -1, ParamTypes.Empty);
+        this.side1 = null;
+        this.side2 = null;
 
         this.inputs = []
 
@@ -155,7 +163,49 @@ class Main {
 
     }
 
+    next_chain() {
+        if (this.current_chain == this.chains[this.chains.length-1]){
+            this.current_chain = this.chains[0];
+            this.iters--;
+        }else{
+            let next_index = this.chains.findIndex((chain)=> chain == this.current_chain)+1;
+            this.current_chain = this.chains[next_index];
+        }
+        if ((this.iters == 0)) {
+            this.end_run();
+        } else {
+            this.prep_chain();
+        }
+    }
 
+    prep_chain() {
+        let old_params: Params = this.current_chain.state(); //Get the last point from the current chain
+        let new_params: Params = null;
+        if (old_params == null) {
+            //If it's empty (a new chain), generate uniformly random values for all parameters for both choices
+            old_params = new Params(
+                Math.floor(Math.random() * 350),
+                Math.floor(Math.random() * 350),
+            );
+            new_params = new Params(
+                Math.floor(Math.random() * 350),
+                Math.floor(Math.random() * 350),
+            );
+        } else {
+            //If you did get a state, create a proposed state by modifying the old one by the proposal distribution
+            new_params = old_params.prop(this.current_chain.prop_var);
+            while (!new_params.isLegal()) {
+                //If you generate out-of-bounds parameters, auto-reject and retry until you get legal ones
+                //console.log("Illegal parameters! Auto-rejecting...")
+                this.current_chain.addPoint(new Result(old_params.auto_copy(), new_params.auto_copy(), true));
+                new_params = old_params.prop(this.current_chain.prop_var);
+            }
+        }
+        this.side1 = old_params;
+        this.side2 = new_params;
+
+        this.render();
+    }
 
     render() {
         function ellipse(context, cx, cy, rx, ry) {
@@ -181,77 +231,56 @@ class Main {
     }
 
     process_input(right: boolean) {
-        let new_state = null;
+        let new_state: Result = null;
         if (right) {
-            new_state = this.side2;
+            new_state = new Result(this.side2, this.side1, false);
             this.inputs.push(true);
         } else {
-            new_state = this.side1;
+            new_state = new Result(this.side1, this.side2, false);
             this.inputs.push(false);
         }
         this.current_chain.addPoint(new_state);
         this.next_chain();
     }
 
-    next_chain() {
-        if (this.current_chain == this.chain_a)
-            this.current_chain = this.chain_b;
-        else if (this.current_chain == this.chain_b)
-            this.current_chain = this.chain_c;
-        else if (this.current_chain == this.chain_c) {
-            this.current_chain = this.chain_a;
-            this.iters--;
-        } else console.error("Something has gone horribly wrong");
-        if ((this.iters == 0)) {
-            this.end_run();
-        } else {
-            this.prep_chain();
+    end_run(){
+        let run_ok: boolean = this.checkout_eval(8, .8);
+        console.log("Done! Run OK: "+run_ok);
+        for(let i = 0; i < this.chains.length; i++){
+            this.chains[i].update(run_ok);
         }
     }
 
-    end_run() {
-        console.log("Done!");
-        console.log(this.inputs.toString());
-    }
-
-    prep_chain() {
-        let old_params = this.current_chain.state(); //Get the last point from the current chain
-        let new_params = null;
-        if (old_params == null) {
-            //If it's empty (a new chain), generate uniformly random values for all parameters for both choices
-            old_params = new Params(
-                Math.floor(Math.random() * 350),
-                Math.floor(Math.random() * 350),
-                ParamTypes.UserDefined
-            );
-            new_params = new Params(
-                Math.floor(Math.random() * 350),
-                Math.floor(Math.random() * 350),
-                ParamTypes.UserDefined
-            );
-        } else {
-            //If you did get a state, create a proposed state by modifying the old one by the proposal distribution
-            new_params = old_params.prop(this.current_chain.prop_var);
-            while (!new_params.isLegal()) {
-                //If you generate out-of-bounds parameters, auto-reject and retry until you get legal ones
-                //console.log("Illegal parameters! Auto-rejecting...")
-                this.current_chain.addPoint(old_params.auto_copy());
-                new_params = old_params.prop(this.current_chain.prop_var);
+    checkout_eval(k: number, t: number) {
+        let alt_len: number = 0;
+        let same_len: number = 0;
+        let left_count = 0;
+        let right_count = 0;
+        let alt_count = 0;
+        let last_val: boolean = null;
+        for(let i: number = 0; i < this.inputs.length; i++){
+            this.inputs[i] ? right_count++ : left_count++;
+            if(this.inputs[i]==last_val){
+                alt_count++;
+                same_len++;
+                alt_len= 0;
+                if(same_len >= k) return false;
+            }else{
+                alt_len++;
+                same_len = 0;
+                last_val = !last_val;
+                if(alt_len >= k) return false;
             }
         }
-        this.side1 = old_params;
-        this.side2 = new_params;
+        if(t <= Math.max(left_count, right_count, alt_count)/this.inputs.length) return false;
 
-        this.render();
-    }
+        return true;
 
-    logKey(e: KeyboardEvent) {
 
     }
 
 
 }
 
-let main = new Main();
-main.iters = 5;
+let main = new Main(5, 5, 10, .8, 5);
 main.prep_chain();
