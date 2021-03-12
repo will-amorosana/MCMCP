52dec1fa2a63bf0e4ce1ae29b4151c796dd7cef2
will-amorosana <will.amorosana@gmail.com>
2021-03-12 10:48:07 +0000

    htfg

--- src/singer.ts
+++ src/singer.ts
@@ -286,6 +286,7 @@ async function process_input(right: boolean) {
 function panic_end(event) {
     end_run(true);
     event.preventDefault();
+    return event.returnValue = "Are you sure you want to exit?";
 }
 
 function end_run(panic: boolean = false) {
